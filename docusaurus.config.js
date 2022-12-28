090ee7d517039333a2b30e44c9008211f6d192bf
reubenliengaard <reubenliengaard@outlook.com>
2022-12-28 11:47:43 +0000

    docs

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -30,20 +30,19 @@ const config = {
 
   presets: [
     [
-      'classic',
-      /** @type {import('@docusaurus/preset-classic').Options} */
-      ({
+      '@docusaurus/preset-classic',
+      {
         docs: {
+          routeBasePath: 'docs',
+          path: 'docs',
           sidebarPath: require.resolve('./sidebars.js'),
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/reubenliengaard/',
+          lastVersion: 'current',
+          onlyIncludeVersions: ['current'],
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
         },
-      }),
+      },
     ],
   ],
 
@@ -95,21 +94,21 @@ const config = {
           },
           {
             to: '/docs-spatial/Intro',    // ./docs-api/Intro.md
-            label: 'API',
+            label: 'Spatial',
             position: 'left',
-            activeBaseRegex: `/docs-api/`,
+            activeBaseRegex: `/docs-spatial/`,
           },
           {
             to: '/docs-parametric/Introducao',  // ./docs-system/Intro.md
-            label: 'My System',
+            label: 'Parametric',
             position: 'left',
-            activeBaseRegex: `/docs-system/`,
+            activeBaseRegex: `/docs-parametric/`,
           },
           {
             to: '/docs-automation/Introducao',  // ./docs-system/Intro.md
-            label: 'My System',
+            label: 'Automation',
             position: 'left',
-            activeBaseRegex: `/docs-system/`,
+            activeBaseRegex: `/docs-automation/`,
           }
         ],
       },
