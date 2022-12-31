bdf2d6275b765c2d8146bde569b2d9a7153e2a8f
reubenliengaard <reubenliengaard@outlook.com>
2022-12-31 04:03:40 +0000

    d

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -44,7 +44,7 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/reubenliengaard',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
@@ -104,7 +104,7 @@ const config = {
               },
               {
                 label: 'GitHub',
-                href: 'https://github.com/facebook/reubenliengaard',
+                href: 'https://github.com/reubenliengaard',
               },
             ],
           },
