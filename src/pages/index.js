776133789fbec781961a2cef7407b6abcc67a9ab
reubenliengaard <reubenliengaard@outlook.com>
2023-01-01 18:47:58 +0000

    d

--- src/pages/index.js
+++ src/pages/index.js
@@ -15,13 +15,11 @@ function HomepageHeader() {
         <h1 className="hero__title">{siteConfig.title}</h1>
         <p className="hero__subtitle">{siteConfig.tagline}</p>
         <p id="p">
-        Welcome to my personal website! My name is Reuben Liengaard and I am a hobbyist with a passion for using technology to solve complex problems and bridge the gap between nature and technology.
+        Welcome to my personal website! My name is Reuben Liengaard and I am an designer with a background in technology and innovation.
 
-I specialize in desktop Linux, environmental monitoring, geospatial analysis, parametric modeling, and artificial intelligence, and my goal is to simplify complexity and make the world a smarter, more sustainable place.
+As a hobbyist, I have a passion for using technology to solve complex problems and bridge the gap between nature and technology. My areas of expertise include desktop Linux, environmental monitoring, geospatial analysis, parametric modeling, and artificial intelligence. My goal is to simplify complexity and make the world a smarter, more sustainable place.
 
-I have always been fascinated by the intersection of nature and technology, and I believe that by bringing these two worlds together, we can create innovative solutions to some of the biggest challenges facing humanity. Whether it's using Linux to build more efficient and secure systems, or using geospatial analysis to understand and protect the environment, I am always looking for new and creative ways to make a positive impact.
-
-On this website, you can learn more about my work, my background, and my passions. I hope you find my content interesting and informative, and I welcome any feedback or questions you may have. Thank you for visiting!
+I am always looking for new and creative ways to make a positive impact, and I believe that by bringing together the worlds of nature and technology, we can find innovative solutions to some of the biggest challenges facing humanity. Thank you for visiting my website, and I hope you find my work and interests interesting and inspiring.
         </p>
         <div className={styles.buttons}>
 
