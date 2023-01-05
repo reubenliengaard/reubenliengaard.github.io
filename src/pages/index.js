10e1664b1da9e22362491b07078b52979f390871
reubenliengaard <reubenliengaard@outlook.com>
2023-01-05 14:00:37 +0000

    h

--- src/pages/index.js
+++ src/pages/index.js
@@ -21,18 +21,17 @@ I have a passion for using computation to solve complex problems.
 
 My areas of expertise include linux, networking, environmental monitoring, geospatial analysis, parametric modeling, and artificial intelligence. 
 
-My goal is to simplify complexity by simple code that deals with large qauntities of data in order to make the world a smarter, more sustainable place.
+My goal is to simplify complexity with clean code that processes large qauntities of data in order to make the world a smarter, more sustainable place.
 
-I am always looking for creative ways to make a positive impact. I believe that by bringing together the worlds of nature and technology, we can find innovative solutions. for example we can identify  heat loss in buildings using infra-red satelite imagery, we can use data querying to find locate wildlife corridors. Thank you for visiting my website, I hope you find my work inspiring.
+I am always looking for creative ways to make a positive impact. I believe that by bringing together the worlds of nature and technology, we can find innovative solutions. for example we can identify  heat loss in buildings using infra-red satelite imagery, or use data querying to locate, protect, or re-instate wildlife corridors.
 
+My professional background encompasses a range of fields, including architecture, and network engineering.
+    
+I gained valuable experience during an internship at Heatherwick Studio, a renowned firm known for its creative and award-winning architectural designs. 
+    
+I also held a position as a network field engineer at Etherlive, a company that offers temporary broadband services for events.
 
-My professional background encompasses a range of fields, including architecture, stone masonry, and network engineering.
-    
-He practices dry stone walling, a time-honored technique for building walls with stones. 
-    
-He gained valuable experience during an internship at Heatherwick Studio, a renowned firm known for its creative and award-winning architectural designs. 
-    
-He has also held a position as a network field engineer at Etherlive, a company that offers temporary broadband services for events.
+Thank you for visiting my website, I hope you find my work inspiring.
         </p>
         <div className={styles.buttons}>
 
