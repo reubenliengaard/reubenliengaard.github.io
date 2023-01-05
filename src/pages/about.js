596aaffc00f81f9940461adbd77c5edc55505a8b
reubenliengaard <reubenliengaard@outlook.com>
2023-01-05 23:35:38 +0000

    f

--- src/pages/about.js
+++ src/pages/about.js
@@ -14,7 +14,13 @@ export default function Hello() {
           padding: '300px'
         }}>
         <p>
-        My name is Reuben Liengaard and I am an designer with a background in applied technology. I have a passion for using computation to solve complex problems. My areas of expertise include linux, networking, environmental monitoring, geospatial analysis, parametric modeling, and artificial intelligence. My goal is to simplify complexity with clean code that processes large qauntities of data in order to make the world a smarter, more sustainable place. I am always looking for creative ways to make a positive impact. I believe that by bringing together the worlds of nature and technology, we can find innovative solutions. for example we can identify  heat loss in buildings using infra-red satelite imagery, or use data querying to locate, protect, or re-instate wildlife corridors. My professional background encompasses a range of fields, including architecture, and network engineering. I gained valuable experience during an internship at Heatherwick Studio, a renowned firm known for its creative and award-winning architectural designs. I also held a position as a network field engineer at Etherlive, a company that offers temporary broadband services for events. Thank you for visiting my website, I hope you find my work inspiring.
+        As a designer with a background in applied technology, I am constantly seeking out new ways to use my skills and knowledge to make a positive impact in the world. My passion for using computation to solve complex problems has led me to become proficient in a variety of areas, including linux, networking, environmental monitoring, geospatial analysis, parametric modeling, and artificial intelligence.
+
+I believe that by combining the worlds of nature and technology, we can find innovative solutions to the challenges facing society today. For example, using infra-red satellite imagery, we can identify heat loss in buildings and take steps to improve energy efficiency. Data querying can be used to locate, protect, or reinstate wildlife corridors, helping to preserve biodiversity and protect the natural world.
+
+In my professional career, I have gained valuable experience in a range of fields, including architecture and network engineering. I have interned at Heatherwick Studio, a renowned architectural design firm, and have held a position as a network field engineer at Etherlive, a company that provides temporary broadband services for events.
+
+I am always looking for creative ways to make a positive impact and am committed to finding solutions to the complex problems facing society today. Thank you for visiting my website, and I hope you find my work inspiring.
         </p>
       </div>
     </Layout>
